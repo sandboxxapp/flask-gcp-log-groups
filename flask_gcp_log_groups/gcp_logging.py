968c3662e1f72250a4f1f14edfb7e0b9a200d5b5
Steven Liu <steven@getbouncer.com>
2019-10-04 18:19:01 -0700

    Fix traceID per latest Google documentation
    Docs: https://cloud.google.com/appengine/docs/flexible/python/writing-application-logs

--- flask_gcp_log_groups/gcp_logging.py
+++ flask_gcp_log_groups/gcp_logging.py
@@ -52,7 +52,9 @@ class GCPHandler(logging.Handler):
         if (self.traceHeaderName in request.headers.keys()):
           # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
           rawTrace = request.headers.get(self.traceHeaderName).split('/')
-          TRACE = rawTrace[0]
+          TRACE = "projects/{project_id}/traces/{trace_id}".format(
+              project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+              trace_id=trace_id)
           if ( len(rawTrace) > 1) :
               SPAN = rawTrace[1].split(';')[0]
 
@@ -81,7 +83,9 @@ class GCPHandler(logging.Handler):
             if (self.traceHeaderName in request.headers.keys()):
               # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
               rawTrace = request.headers.get(self.traceHeaderName).split('/')
-              TRACE = rawTrace[0]
+              TRACE = "projects/{project_id}/traces/{trace_id}".format(
+                  project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+                  trace_id=trace_id)
               if ( len(rawTrace) > 1) :
                 SPAN = rawTrace[1].split(';')[0]
 

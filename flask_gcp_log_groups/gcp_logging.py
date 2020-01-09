305e8d0cb50c15f8ab3f18ca29e7ec56d6d09096
Eric Pluntze <eric@sandboxx.us>
2020-01-09 13:40:48 -0500

    abandon request parent log

--- flask_gcp_log_groups/gcp_logging.py
+++ flask_gcp_log_groups/gcp_logging.py
@@ -2,15 +2,12 @@
 # Use of this source code is governed by the Apache 2.0
 # license that can be found in the LICENSE file.
 
-import logging
-import json
 import datetime
+import logging
 import os
 
-import time
-from flask import Flask, has_request_context
-from flask import request, Response, render_template, g, jsonify, current_app
-
+from flask import has_request_context
+from flask import request
 from google.cloud import logging as gcplogging
 from google.cloud.logging.resource import Resource
 
@@ -23,8 +20,8 @@ client = gcplogging.Client()
 
 class GCPHandler(logging.Handler):
     
-    def __init__(self, app, parentLogName='request', childLogName='application', 
-                traceHeaderName=None,labels=None, resource=None):
+    def __init__(self, app, childLogName='application',
+                 traceHeaderName=None, labels=None, resource=None):
         logging.Handler.__init__(self)
         self.app = app
         self.labels=labels
@@ -34,11 +31,8 @@ class GCPHandler(logging.Handler):
         else:
             resource = Resource(type=resource['type'], labels=resource['labels'])
         self.resource = resource
-        self.transport_parent = BackgroundThreadTransport(client, parentLogName)
         self.transport_child = BackgroundThreadTransport(client, childLogName)           
         self.mLogLevels = []
-        if app is not None:
-            self.init_app(app)
             
     def emit(self, record):
         if not has_request_context():
@@ -67,70 +61,3 @@ class GCPHandler(logging.Handler):
                 labels=self.labels,
                 trace=TRACE,
                 span_id=SPAN)            
-
-    def init_app(self, app):
-
-        # capture the http_request time
-        @app.before_request
-        def before_request():
-            g.request_start_time = time.time()
-            g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
-
-        # always log the http_request@ default INFO
-        @app.after_request
-        def add_logger(response):
-            TRACE = None
-            SPAN = None
-            if (self.traceHeaderName in request.headers.keys()):
-                # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
-                rawTrace = request.headers.get(self.traceHeaderName).split('/')
-                trace_id = rawTrace[0]
-                TRACE = "projects/{project_id}/traces/{trace_id}".format(
-                  project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-                  trace_id=trace_id)
-                if ( len(rawTrace) > 1):
-                    SPAN = rawTrace[1].split(';')[0]
-                logging.error(request.headers.get(self.traceHeaderName))
-                logging.error(rawTrace)
-                logging.error(trace_id)
-                logging.error(TRACE)
-                logging.error(SPAN)
-
-            # https://github.com/googleapis/googleapis/blob/master/google/logging/type/http_request.proto
-            REQUEST = {
-                'requestMethod': request.method,
-                'requestUrl': request.url,
-                'status': response.status_code,
-                'responseSize': response.content_length,
-                'latency': g.request_time(),
-                'remoteIp': request.remote_addr,
-                'requestSize': request.content_length
-            }
-
-            if 'user-agent' in request.headers:
-                REQUEST['userAgent'] = request.headers.get('user-agent')
-
-            if request.referrer:
-                REQUEST['referer'] = request.referrer
-
-            # find the log level priority sub-messages; apply the max level to the root log message
-            if len(self.mLogLevels) == 0:
-                severity = logging.getLevelName(logging.INFO)
-                if (response.status_code >= 400 and response.status_code < 500):
-                   severity = logging.getLevelName(logging.WARNING)
-                elif (response.status_code >= 500):
-                   severity = logging.getLevelName(logging.ERROR)
-            else:
-                severity = logging.getLevelName(max(self.mLogLevels))
-            self.mLogLevels=[]
-            self.transport_parent.send(
-                None,
-                timestamp=datetime.datetime.utcnow(),
-                severity=severity,
-                resource=self.resource,
-                labels=self.labels,
-                trace=TRACE,
-                span_id=SPAN,
-                http_request=REQUEST)
-
-            return response
